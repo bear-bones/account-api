3ec4df0b34c74b02f7aaa68650432a34dde96b52
James White <james@thewhites.co>
2022-03-29 19:40:25 -0700

    finished infrastructure

--- config/config.py
+++
@@ -1,2 +0,0 @@
-MONGODB_URL = 'mongodb+srv://cluster0.pzkiu.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority'
-MONGODB_CERT = 'config/mongodb-atlas.crt'
